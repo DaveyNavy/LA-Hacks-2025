d747aafb14e04919528755389357990811a1b8de
kennedy kang <kennedyekang@gmail.com>
2025-04-26 13:55:11 -0700

    added add task,  delete task, update task, complete task

--- backend/routes/tasksRouter.js
+++ backend/routes/tasksRouter.js
@@ -1,8 +1,12 @@
 import Router from "express";
-import { tasksPageGet } from "../controllers/tasksController.js";
+import { tasksPageGet, tasksPagePost, tasksPageDelete, tasksPagePut, tasksCompletePost } from "../controllers/tasksController.js";
 import { verifyToken } from "./utils.js";
 const tasksRouter = Router();
 
 tasksRouter.get("/", verifyToken, tasksPageGet);
+tasksRouter.post("/", verifyToken, tasksPagePost);
+tasksRouter.delete("/:taskId", verifyToken, tasksPageDelete);  
+tasksRouter.put("/:taskId", verifyToken, tasksPagePut);
+tasksRouter.post("/:taskId/complete", verifyToken, tasksCompletePost);
 
 export default tasksRouter;
